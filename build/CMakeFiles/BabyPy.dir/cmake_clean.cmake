a5ca255c6eaa6ec17424b9883e89639bcb17a055
CoderMANE321 <108749886+CoderMANE321@users.noreply.github.com>
2025-11-14 15:54:02 +0000

    Built out foundation of bytecode and memory management.

---
+++ build/CMakeFiles/BabyPy.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "BabyPy"
+  "BabyPy.pdb"
+  "CMakeFiles/BabyPy.dir/src/chunk.cpp.o"
+  "CMakeFiles/BabyPy.dir/src/chunk.cpp.o.d"
+  "CMakeFiles/BabyPy.dir/src/main.cpp.o"
+  "CMakeFiles/BabyPy.dir/src/main.cpp.o.d"
+  "CMakeFiles/BabyPy.dir/src/memory.cpp.o"
+  "CMakeFiles/BabyPy.dir/src/memory.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BabyPy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
